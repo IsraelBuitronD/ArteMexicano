4a3e50f60b7a33ad40c36a67c6141103ca88f130
Israel Buitron <israel.buitron@gmail.com>
2012-10-22 15:25:53 -0500

    Rails scaffold added.
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArteMexicano::Application.config.secret_token = '68846df8dfb0c5f94259caff0d4f1027fbfdfef0872bc314aaa1d55a9b8b5ae6886f2a43065b18a519de4b8abf197f3f0d3d938fb976f130c384ecab7d608c5d'

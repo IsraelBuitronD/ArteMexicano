5ba22bc2fd2d47d03ba2e4ba3f01e05b411f073a
Israel Buitron <israel.buitron@gmail.com>
2012-10-23 21:57:32 -0500

    ActsAsTaggableOnMigration was generated.
    Notice:
    - This commit adds a migration, please run `rake db:migrate` to keep updated your database schema.
    
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121023174003) do
+ActiveRecord::Schema.define(:version => 20121024020143) do
 
   create_table "artists", :force => true do |t|
     t.string  "name"
@@ -80,13 +80,6 @@ ActiveRecord::Schema.define(:version => 20121023174003) do
 
   add_index "handcrafts_photos", ["handcraft_id", "photo_id"], :name => "handcrafts_photos_index", :unique => true
 
-  create_table "handcrafts_tags", :id => false, :force => true do |t|
-    t.integer "handcraft_id"
-    t.integer "tag_id"
-  end
-
-  add_index "handcrafts_tags", ["handcraft_id", "tag_id"], :name => "handcrafts_tags_index", :unique => true
-
   create_table "handcrafts_techniques", :id => false, :force => true do |t|
     t.integer "handcraft_id"
     t.integer "technique_id"
@@ -125,6 +118,19 @@ ActiveRecord::Schema.define(:version => 20121023174003) do
   create_table "states", :force => true do |t|
     t.string "name"
   end
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       :limit => 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
 
   create_table "tags", :force => true do |t|
     t.string "name"

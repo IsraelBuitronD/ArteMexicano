1c8b6f4d58ad479a4ea1afa3fdeebbfbd4bc9921
Israel Buitron <israel.buitron@gmail.com>
2012-10-23 23:30:02 -0500

    Removed many-to-many relation between `handcraft` and `tags`.
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

--- app/models/handcraft.rb
+++ app/models/handcraft.rb
@@ -8,5 +8,4 @@ class Handcraft < ActiveRecord::Base
   has_and_belongs_to_many :materials
   has_and_belongs_to_many :manufacturing_techniques
   has_and_belongs_to_many :photos
-  has_and_belongs_to_many :tags
 end

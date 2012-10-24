154ddaed6984b173cd7cd2d50f771a6fafa7e4b6
Israel Buitron <israel.buitron@gmail.com>
2012-10-23 23:24:15 -0500

    Mark `handcraft` model with `acts_as_taggable`.
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

--- app/models/handcraft.rb
+++ app/models/handcraft.rb
@@ -1,6 +1,8 @@
 class Handcraft < ActiveRecord::Base
   attr_accessible :artist_id, :depth, :description, :height, :highlight, :name, :status, :weight, :width, :tag_list
 
+  acts_as_taggable
+
   belongs_to :artist
   has_and_belongs_to_many :techniques
   has_and_belongs_to_many :materials

7ad139a0fb05dcf816bf9b2fda3fc4f45cc15076
Israel Buitron <israel.buitron@gmail.com>
2012-11-09 13:06:15 -0600

    Devise authentication was added.
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ArteMexicano::Application.routes.draw do
 
+  devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout'}
+
   get "home/index"
 
   get 'tags/:tag', to: 'handcrafts#index', as: :tag
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121104203000) do
+ActiveRecord::Schema.define(:version => 20121109184051) do
 
   create_table "artists", :force => true do |t|
     t.string  "name"
@@ -141,5 +141,14 @@ ActiveRecord::Schema.define(:version => 20121104203000) do
     t.string "name"
     t.string "description"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",              :default => "", :null => false
+    t.string   "encrypted_password", :default => "", :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end

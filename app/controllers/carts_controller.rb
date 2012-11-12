c7919fbc62ade334b5b70076190488d4d7274e72
Israel Buitron <israel.buitron@gmail.com>
2012-11-12 13:30:17 -0600

    Updated redirections in Cart controller.
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -17,7 +17,7 @@ class CartsController < ApplicationController
       @cart = Cart.find(params[:id])
     rescue ActiveRecord::RecordNotFound
       logger.error "Attempt to access invalid cart #{params[:id]}"
-      redirect_to root_url, :notice => 'Invalid cart'
+      redirect_to handcrafts_url, :notice => 'Invalid cart'
     else
       respond_to do |format|
         format.html # show.html.erb
@@ -79,9 +79,10 @@ class CartsController < ApplicationController
   def destroy
     @cart = Cart.find(params[:id])
     @cart.destroy
+    session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to carts_url }
+      format.html { redirect_to handcrafts_url, notice: 'Your cart is currently empty' }
       format.json { head :no_content }
     end
   end

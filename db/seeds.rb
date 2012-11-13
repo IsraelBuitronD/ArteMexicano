3f47a8063c01db5625a4f85518565678e7081811
Ricardo Vega <ricardoe@devluv.com>
2012-11-12 21:53:30 -0600

    Changes to seeds.db again

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,50 @@
 # encoding: utf-8
-State.create(name: 'Aguascalientes')
-State.create(name: 'Baja California')
-State.create(name: 'Baja California Sur')
-State.create(name: 'Campeche')
-State.create(name: 'Chiapas')
-State.create(name: 'Chihuahua')
-State.create(name: 'Coahuila de Zaragoza')
-State.create(name: 'Colima')
-State.create(name: 'Durango')
-State.create(name: 'Guanajuato')
-State.create(name: 'Guerrero')
-State.create(name: 'Hidalgo')
-State.create(name: 'Jalisco')
-State.create(name: 'Michoacan de Ocampo')
-State.create(name: 'Morelos')
-State.create(name: 'Nayarit')
-State.create(name: 'Nuevo Leon')
-State.create(name: 'Oaxaca')
-State.create(name: 'Puebla')
-State.create(name: 'Queretaro')
-State.create(name: 'Quintana Roo')
-State.create(name: 'San Luis Potosi')
-State.create(name: 'Sinaloa')
-State.create(name: 'Sonora')
-State.create(name: 'Tabasco')
-State.create(name: 'Tamaulipas')
-State.create(name: 'Tlaxcala')
-State.create(name: 'Veracruz de Ignacio de Llave')
-State.create(name: 'Yucatan')
-State.create(name: 'Zacatecas')
-
-User.create(email: 'admin@artepopular.com.mx', password: 'admin')
-
 Artist.create([
-  { :name => "Cesar Duranton", :business_name => "Duranton", :address => "Mexico, DF", :phone => "5512341234", :email => "cesar.duranton@artepopular.com.mx", :foto => "cesarduranton.jpg", :latitude => 19.432661, :longitud => -99.13334, :location => "Mexico, DF", :biography => "<p><span style=\"color: #777777; font-family: Georgia, 'Times New Roman', serif; font-size: 13px; font-style: italic; line-height: 18px;\">Trabajo dentro del arte popular mexicano, desde hace 15 a&ntilde;os elaboro piezas piezas tradicionales y otras de dise&ntilde;o propio en cartoner&iacute;a.</span></p>" }
+  { :name => "Cesar Duranton", :business_name => "Duranton", :address => "Mexico, DF", :phone => "5512341234", :email => "cesar.duranton@artepopular.com.mx", :foto => "cesarduranton.jpg", :latitude => 19.432661, :longitud => -99.13334, :location => "Mexico, DF", :biography => "<p>Trabajo dentro del arte popular mexicano, desde hace 15 a&ntilde;os elaboro piezas piezas tradicionales y otras de dise&ntilde;o propio en cartoner&iacute;a.</p>" },
+  { :name => "Jorge Rosano", :business_name => "Papel Recortado", :address => "Tepoztlan, Morelos, Mexico", :phone => "12341234", :email => "jorge.rosano@artepopular.com.mx", :foto => "jorgerosano.jpg", :latitude => 18.9856, :longitud => -99.09978, :location => "", :biography => "<p><em>\"Las formas aparecen y yo quito lo que sobra\"</em></p>\r\n<p>Jorge Rosano</p>\r\n<p><em>\"Mi arte est&aacute; relacionado con el papel recortado de los ind&iacute;genas otom&iacute;es (&ntilde;a&ntilde;u) herederos de las culturas prehisp&aacute;nicas m&aacute;s antiguas; no con el papel picado, una t&eacute;cnica que viene de China\"</em></p>\r\n<p>Jorge Rosano</p>" }
+], :without_protection => true )
+
+State.create([
+  { :name => "Aguascalientes" },
+  { :name => "Baja California" },
+  { :name => "Baja California Sur" },
+  { :name => "Campeche" },
+  { :name => "Chiapas" },
+  { :name => "Chihuahua" },
+  { :name => "Coahuila de Zaragoza" },
+  { :name => "Colima" },
+  { :name => "Durango" },
+  { :name => "Distrito Federal" },
+  { :name => "Guanajuato" },
+  { :name => "Guerrero" },
+  { :name => "Hidalgo" },
+  { :name => "Jalisco" },
+  { :name => "Michoacan de Ocampo" },
+  { :name => "Morelos" },
+  { :name => "Nayarit" },
+  { :name => "Nuevo Leon" },
+  { :name => "Oaxaca" },
+  { :name => "Puebla" },
+  { :name => "Queretaro" },
+  { :name => "Quintana Roo" },
+  { :name => "San Luis Potosi" },
+  { :name => "Sinaloa" },
+  { :name => "Sonora" },
+  { :name => "Tabasco" },
+  { :name => "Tamaulipas" },
+  { :name => "Tlaxcala" },
+  { :name => "Veracruz de Ignacio de Llave" },
+  { :name => "Yucatan" },
+  { :name => "Zacatecas" }
 ], :without_protection => true )
 
 Technique.create([
-  { :name => "Cartonería", :description => "<p>La Cartoner&iacute;a es considerada un arte popular mexicano, consiste en una t&eacute;cnica con la que se crean figuras a trav&eacute;s de un proceso de modelado de papel peri&oacute;dico.</p>\r\n<p>Entre los m&aacute;s relevantes tipos de piezas creadas con esta t&eacute;cnica est&aacute;n los llamados judas diablitos, pi&ntilde;atas, toritos, mojigangas, alebrijes, catrinas y calaveras, viejitos y panzones, as&iacute; como m&aacute;scaras y juguetes hechos de papel entre los que destacan la &ldquo;lolita&rdquo; y los caballitos.</p>" }
+  { :name => "Cartonería", :description => "<p>La Cartoner&iacute;a es considerada un arte popular mexicano, consiste en una t&eacute;cnica con la que se crean figuras a trav&eacute;s de un proceso de modelado de papel peri&oacute;dico.</p>" },
+  { :name => "Papel Recortado", :description => "<p>Papel recortado sin dise&ntilde;o previo, entintado por la orilla.</p>" }
+], :without_protection => true )
+
+
+
+User.create([
+  { :email => "admin@artepopular.com.mx", :encrypted_password => "$2a$10$cDposMV2TGweLWb1nDZDB.6Mu.mrPEY38Uqv5YNUTIe8ASww9tqri", :created_at => "2012-11-13 02:34:56", :updated_at => "2012-11-13 02:34:56" }
 ], :without_protection => true )
